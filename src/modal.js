037caf09aa7e74a93f6e4b3258c41656d341963c
giovanniinfobasic <giovanni.infobasic@gmail.com>
2024-01-15 18:15:04 +0100

    Co-authored-by: Marco <tekno2.0developer@gmail.com>

--- src/modal.js
+++ src/modal.js
@@ -1,6 +1,12 @@
 const modalBtn = document.querySelector("#modalBtn");
+const modalBtnClose = document.querySelector("#modalBtnClose");
+const modalScontrino = document.querySelector("#modalScontrino");
 
 function openModal() {
-    
+    modalScontrino.classList.remove("hidden");
+}
+function closeModal() {
+    modalScontrino.classList.add("hidden");
 }
 modalBtn.addEventListener("click", openModal);
+modalBtnClose.addEventListener("click", closeModal);
