2afc6a8a896a947c5b30c702a6985803ca669e48
giovanniinfobasic <giovanni.infobasic@gmail.com>
2024-01-15 18:33:38 +0100

    update

--- src/modal.js
+++ src/modal.js
@@ -1,12 +1,16 @@
+//Dichiaro le variabili da usare per il modal dello scontrino
 const modalBtn = document.querySelector("#modalBtn");
 const modalBtnClose = document.querySelector("#modalBtnClose");
 const modalScontrino = document.querySelector("#modalScontrino");
 
+//Funzione che apre il modal
 function openModal() {
     modalScontrino.classList.remove("hidden");
 }
+//Funzione che chiude il modal
 function closeModal() {
     modalScontrino.classList.add("hidden");
 }
+//Quando aprire e chiudere il modal
 modalBtn.addEventListener("click", openModal);
 modalBtnClose.addEventListener("click", closeModal);
